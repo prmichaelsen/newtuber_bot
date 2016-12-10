0dcad1941357fc4f49ed13e7fbb2002489fa6363
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2016-12-09 20:03:07 -0700

    added some err handling

--- newtuber_bot.py
+++ newtuber_bot.py
@@ -26,9 +26,13 @@ for submission in r.get_subreddit('newtubers').get_new(limit=5):
     if submission.id in ignore:
       pass
     else:
-      ignore[submission.id] = submission.id;
       body = "#This is a critique thread! Here's a friendly reminder about the rules for a critique:\r\n\r\n * **2a** - In order to post your plug in a Critique thread, you **must** give meaningful feedback on at least **TWO (2)** other plugs in the thread. You should give feedback **before** posting your own plug.  \r\n\r\n * **2b** - In cases where you are the first or second plug post in a Critique thread, you may post your link before giving feedback, so long as you give feedback within ONE HOUR of your initial link. If you don't give feedback within an hour, your post will be removed. If you're a repeat offender, you will be marked as such and prohibited from posting links until your behavior improves.  \r\n\r\n See the [full rules here](https://goo.gl/qs5HAn) or in the sidebar"
-      submission.add_comment(body);
+      try:
+        success = submission.add_comment(body)
+        if success:
+          ignore[submission.id] = submission.id;
+      except RateLimitExceeded:
+        print('Rate limit exceeded, could not post to submission id: %s' % submission.id)
       pass
 
 with open(config_file, 'w') as wh:
